106927789786581dcadeab383ccd2615f0cf2b65
AL-QAZWENY saif <alqazwen@informatique.univ-paris-diderot.fr>
2017-04-06 16:01:53 +0200

    ajouter les fichiers : conduct.c , conduct.h et julia.c

---
+++ conduct.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <semaphore.h>
+
+struct conduct{
+    size_t atomicity;
+    size_t capacity;
+    char* name;
+    char* buff;
+};
+
+struct conduct *conduct_create(const char *name, size_t a, size_t c);
+struct conduct *conduct_open(const char *name);
+ssize_t conduct_read(struct conduct *c, void *buf, size_t count);
+ssize_t conduct_write(struct conduct *c, const void *buf, size_t count);
+int conduct_write_eof(struct conduct *c);
+void conduct_close(struct conduct *conduct);
+void conduct_destroy(struct conduct *conduct);
