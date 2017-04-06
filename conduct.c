1380399867f6675154550aa3e44814d6f742ecb8
AL-QAZWENY saif <alqazwen@informatique.univ-paris-diderot.fr>
2017-04-06 17:27:35 +0200

    conduct_create avec name!=null

--- conduct.c
+++ conduct.c
@@ -1,11 +1,24 @@
 #include "conduct.h"
 
-struct conduct * conduct_create(char *name,size_t c,size_t a){
-    struct conduct* conduct=NULL;
+struct conduct *conduct_create(const char *name,size_t c,size_t a){
+    struct conduct* conduit=NULL;
     if(name!=NULL){
-        conduct=malloc(sizeof(struct conduct));
-        conduct->buff=malloc(sizeof(char)*c);
-        conduct->capacity=c;
-        conduct->atomicity=a;
+        conduit=malloc(sizeof(struct conduct));
+        if(conduit==NULL){
+            perror("Allocation dynamique struct conduct a echoue");
+            exit(1);
+        }
+        conduit->buff=malloc(sizeof(char)*c);
+        if(conduit->buff==NULL){
+            perror("Allocation dynamique buff a echoue");
+            exit(2);
+        }
+        conduit->capacity=c;
+        conduit->atomicity=a;
+        conduit->fd=open(name,O_RDWR);
+        if(conduit->fd==-1){
+            perror("Ouverture du fichier a echoue");
+            exit(3);
+        }
     }
 }
--- conduct.h
+++ conduct.h
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <semaphore.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
 
 /*Structure Conduit */
 struct conduct{
