381acd4aba264a21eb328236f090ed06e3e535b2
adonis <elkhoury@informatique.univ-paris-diderot.fr>
2017-04-07 20:34:10 +0200

    correction create

--- main.c
+++ main.c
@@ -3,6 +3,6 @@
 int main(int argc, char const *argv[]) {
     struct conduct *cond1,*cond2;
     cond1=conduct_create("saif", 10, 20);
-    cond2=conduct_open("saif");
+    //cond2=conduct_open("saif");
     return 0;
 }
