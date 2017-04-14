c0a1e4042bb90c339504ef7d83203bad19f656ec
AL-QAZWENY saif <alqazwen@informatique.univ-paris-diderot.fr>
2017-04-14 14:48:01 +0200

    c'etatit pour le test

--- main2.c
+++
@@ -1,9 +0,0 @@
-#include "conduct.h"
-
-int main(int argc, char const *argv[]) {
-    struct conduct *cond1,*cond2;
-    //cond1=conduct_create("saif", 20, 10);
-    cond2=conduct_open("saif");
-
-    return 0;
-}
